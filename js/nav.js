a5cbebe1d2e91beec3d5f4a1cf60578be996c257
MrNedNick <mrnediala@gmail.com>
2021-04-20 15:36:10 +0300

    Add fixed nav

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    let nav = $("#nav"),
+        headerH = $("#header").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    // fixed nav
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+        
+});
+
+    function checkScroll () {
+        if(scrollOffset >= headerH) {
+            nav.addClass("fixed");
+        } else {
+            nav.removeClass("fixed");
+        }
+    }
+
+});
