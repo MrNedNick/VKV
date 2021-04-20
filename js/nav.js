3e7a97ff69bca930f5332e0414bb3943143047ec
MrNedNick <mrnediala@gmail.com>
2021-04-20 19:24:39 +0300

    Chage fixed nav

--- js/nav.js
+++ js/nav.js
@@ -14,11 +14,39 @@ $(function() {
         
 });
 
+
     function checkScroll () {
         if(scrollOffset >= headerH) {
             nav.addClass("fixed");
         } else {
             nav.removeClass("fixed");
+        }
+    }
+
+});
+
+$(function() {
+
+    let about = $("#about"),
+        headerH = $("#header").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    // fixed about
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+        
+});
+
+
+    function checkScroll () {
+        if(scrollOffset >= headerH) {
+            about.addClass("fixed");
+        } else {
+            about.removeClass("fixed");
         }
     }
 
