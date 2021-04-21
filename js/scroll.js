bf708a116852fe52a2141756e62d7745d13ac130
MrNedNick <mrnediala@gmail.com>
2021-04-21 13:00:20 +0300

    Fix bugs

--- js/scroll.js
+++ js/scroll.js
@@ -9,20 +9,28 @@ $(function() {
     
 
     $(window).on('scroll', function () {
-        
+
         scrollOffset = $(this).scrollTop();
-        if (scrollOffset >= 1 && scrollOffset < 120) {
+        if (scrollOffset >= 50 && scrollOffset < 150) {
             wrapper.style.transform = 'scale(1.5)';
-        } else if(scrollOffset >= 120 && scrollOffset < 200) {
-            wrapper.style.transform = 'scale(2)';
-        } else if(scrollOffset >= 200 && scrollOffset < 300) {
-            wrapper.style.transform = 'scale(2.5)';
-        } else if(scrollOffset >= 300 && scrollOffset < 400) {
-            wrapper.style.transform = 'scale(3)';
-        } else if(scrollOffset >= 400 && scrollOffset < 500) {
-            wrapper.style.transform = 'scale(3.5)';
-        } else if(scrollOffset >= 500) {
-            wrapper.style.transform = 'scale(4)',
+        } else if(scrollOffset >= 150 && scrollOffset < 250) {
+            wrapper.style.transform = 'scale(2)',
+            wrapper.style.opacity = 1,
+            nav.style.opacity = 0;;
+        } else if(scrollOffset >= 250 && scrollOffset < 350) {
+            wrapper.style.transform = 'scale(2.5)',
+            wrapper.style.opacity = 1,
+            nav.style.opacity = 0;;
+        } else if(scrollOffset >= 350 && scrollOffset < 450) {
+            wrapper.style.transform = 'scale(3)',
+            wrapper.style.opacity = 1,
+            nav.style.opacity = 0;;
+        } else if(scrollOffset >= 450 && scrollOffset < 550) {
+            wrapper.style.transform = 'scale(3.5)',
+            wrapper.style.opacity = 1,
+            nav.style.opacity = 0;;
+        } else if(scrollOffset >= 550) {
+            wrapper.style.transform = 'scale(4.0)',
             wrapper.style.opacity = 0,
             nav.style.opacity = 1;
         } else {wrapper.style.transform = 'scale(1.0)',
