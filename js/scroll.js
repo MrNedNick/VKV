116791ab4a02589d76ff9181e8deeb348193551a
MrNedNick <mrnediala@gmail.com>
2021-04-21 12:20:12 +0300

    Fixed bugs in "Header"

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,7 @@ $(function() {
     let headerH = $('#header').innerHeight();
 
     let wrapper = document.querySelector('#wrapper');
+    let nav = document.querySelector('#nav');
     let scrollOffset = 0;
 
     
@@ -10,28 +11,23 @@ $(function() {
     $(window).on('scroll', function () {
         
         scrollOffset = $(this).scrollTop();
-        if (scrollOffset >= 100 && scrollOffset < 200) {
+        if (scrollOffset >= 1 && scrollOffset < 120) {
             wrapper.style.transform = 'scale(1.5)';
-        } else if(scrollOffset >= 200 && scrollOffset < 300) {
+        } else if(scrollOffset >= 120 && scrollOffset < 200) {
             wrapper.style.transform = 'scale(2)';
-        } else if(scrollOffset >= 300 && scrollOffset < 400) {
+        } else if(scrollOffset >= 200 && scrollOffset < 300) {
             wrapper.style.transform = 'scale(2.5)';
-        } else if(scrollOffset >= 400 && scrollOffset < 500) {
+        } else if(scrollOffset >= 300 && scrollOffset < 400) {
             wrapper.style.transform = 'scale(3)';
-        } else if(scrollOffset >= 500 && scrollOffset < 600) {
+        } else if(scrollOffset >= 400 && scrollOffset < 500) {
             wrapper.style.transform = 'scale(3.5)';
-        } else if(scrollOffset >= 600 && scrollOffset < 700) {
-            wrapper.style.transform = 'scale(4)';
-        } else if(scrollOffset >= 800 && scrollOffset < 900) {
-            wrapper.style.transform = 'scale(4.5)';
-        } else if(scrollOffset >= 1000 && scrollOffset < 1100) {
-            wrapper.style.transform = 'scale(5)';
-        } else if(scrollOffset >= 1100 && scrollOffset < 1200) {
-            wrapper.style.transform = 'scale(5.5)';
-        } else {wrapper.style.transform = 'scale(1.0)';}
-
-
-        console.log(scrollOffset);
+        } else if(scrollOffset >= 500) {
+            wrapper.style.transform = 'scale(4)',
+            wrapper.style.opacity = 0,
+            nav.style.opacity = 1;
+        } else {wrapper.style.transform = 'scale(1.0)',
+                wrapper.style.opacity = 1,
+                nav.style.opacity = 0;}
     });
 
 });
