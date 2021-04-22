4c976ed4187ba5531470266201db439cd11e6aa3
MrNedNick <mrnediala@gmail.com>
2021-04-22 10:04:20 +0300

    Add js navigation

--- js/nav.js
+++ js/nav.js
@@ -52,3 +52,18 @@ $(function() {
 
 });
 
+
+
+// smooth scroll
+$("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this),
+        blockId = $this.data('scroll'),
+        blockOffset = $(blockId).offset().top;
+
+    $("html, body").animate({
+        scrollTop: blockOffset
+    }, 500);
+});
+
